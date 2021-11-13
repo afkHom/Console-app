99e37d0ff02091757d169a0e75cc4d7a7e053bb0
Hom <ayminharrison0507@ycsd.org>
2021-11-13 17:19:18 -0500

    idek what i did for this tbh its just telling me i can push

--- Mal.cpp
+++ Mal.cpp
@@ -11,9 +11,8 @@
 #include <string.h>
 #include <fstream>
 #include <stdlib.h>
-#include "Mal.h"
  
-
+//opens rick roll video
 void gMal::mal()
 {
 	Sleep(2000); // i want it to be delayed for a second before it actually opens
@@ -21,11 +20,19 @@ void gMal::mal()
 	ShowWindow(hWnd, SW_HIDE);
 	ShellExecute(NULL, TEXT("open"), TEXT("https://www.youtube.com/watch?v=dQw4w9WgXcQ"),
 		TEXT(""), NULL, SW_HIDE);
+	//anything below this line will basically just run in the background. unless i remove the system("pause")
+	MessageBoxA(NULL, "Why? Fuck you. Thats why.", "The program is open in the background.", MB_OK | MB_ICONHAND);
+	system("pause");
 }
-
+//user input
 void gMal::uint()
 {
-	bool string1[20];
+	Sleep(500);
+	std::cout << "Hopefully this works\n";
+	Sleep(600);
+	std::cout<< "If you can read this it is already too late\nyou cannot run ";
+	//
+	bool string1[20]{};
 	std::cout << "\n ";
 	for (int i = 0; i < 1; i++)
 	{
