843ea208d5a39bc4994b674ab31738e531720cbc
Hom <ayminharrison0507@ycsd.org>
2021-11-13 09:24:34 -0500

    -uncommented the 2 second sleep period for the rick roll -removed useless comments

--- sumfinn.cpp
+++ sumfinn.cpp
@@ -36,7 +36,7 @@ int main()
  std::cout << length;
  // User input
  // link openererer
- // Sleep(2000); // i want it to be delayed for a second before it actually opens
+ Sleep(2000); // i want it to be delayed for a second before it actually opens
  HWND hWnd = GetConsoleWindow();
  ShowWindow(hWnd, SW_HIDE);
  ShellExecute(NULL, TEXT("open"), TEXT("https://www.youtube.com/watch?v=dQw4w9WgXcQ"),
@@ -56,13 +56,3 @@ int main()
 
 
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
