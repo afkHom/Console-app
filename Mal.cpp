21b6dbbe38aeb1efd7c80232ff621bb5e9a92ec6
Hom <71136930+afkHom@users.noreply.github.com>
2022-12-08 12:31:37 -0500

    revised accptdeny so its more secure

--- Mal.cpp
+++ Mal.cpp
@@ -48,31 +48,27 @@ void gMal::logo()
 	system("cls");
 }
 void gMal::accptdeny()
-{// A=0x41 D=0x44
-	system("cls");
-	cout << "Accept or Deny here please\n:";
-	cin >> accpt;
- if(accpt== 'A' || accpt== 'a')
- {
-	 cout << "accepted, proceeding";
-	 Sleep(1000);
-	 cout << ".";
-	 Sleep(1000);
-	 cout << ".";
-	 Sleep(1000);
-	 cout << ".";//theres probably a better way of doing this, but i just genuinelly dont care. Its 1:51 AM
-	 jesus.question();
-	 //jesus.link(); I want this to be the last used thing, i just dont know where itll end yet
- }
- else if(accpt== 'D' || accpt== 'd')
- {
-	 cout << "denied\n";
-	 cout << "shutting down";
-	 Sleep(1000);
-	 system("taskkill /IM sumfinn.exe /F");
-	 
- }
+{
+	// A=0x41 D=0x44
+	char accpt = '\0';
+	while (accpt != 'A' && accpt != 'D')
+	{
+		std::cout << "Accept or Deny here please\n:";
+		std::cin >> accpt;
+	}
+
+	if (accpt == 'A')
+	{
+		std::cout << "accepted, proceeding" << std::endl;
+		jesus.question();
+	}
+	else if (accpt == 'D')
+	{
+		std::cout << "denied" << std::endl;
+		return;
+	}
 }
+
 
 //------------------------------------------------------------------------------------------idk how this is going to work. this is probably going to be very messy game of back and forth between files
 void gMal::questions()
